87ac861a161edca2d960468f26ab9341dba7b868
cqktss0 <JadhavGanesh2@Johndeere.com>
2020-05-04 14:15:53 +0530

    webpack example

--- webpack-react/app.js
+++ webpack-react/app.js
@@ -4,6 +4,12 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(path.join(__dirname, '\\dist\\app')));
+// An api endpoint that returns a short list of items
+app.get('/api/getList', (req,res) => {
+  var list = ["item1", "item2", "item3"];
+  res.json(list);
+  console.log('Sent list of items');
+});
 
 app.get('/*', function(req, res){
   res.sendFile("index.html", {root: path.join(__dirname, '\\dist')});
